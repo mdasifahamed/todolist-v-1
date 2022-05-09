852d45f85f31e7a434e4256731c2a7e19a09b3af
asif <mdasifahamed89@yahoo.com>
2022-05-09 15:06:47 +0600

    Adding New route and creating more page with single ejs templates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose'); // package for dtabase
-const date = require(__dirname + "/date.js"); //  external dat moudule
+const date = require(__dirname + "/date.js"); //  external date moudule
 const ObjectId = require('mongodb').ObjectID;
 const app = express();
 const port = 3000;
@@ -12,11 +12,18 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}); // setting the database
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+}); // setting the database
 const ItemSchema = new mongoose.Schema({ // creatingt data table or for mongoose it says documents format
   name: String
 });
 const Item = mongoose.model("Item", ItemSchema); // setting the data model
+const listSchema = { // mongoose schema for custom lists and gonna conta itemschema data
+  name: String,
+  item: [ItemSchema]
+};
+const List = mongoose.model("List", listSchema); // creating the model
 
 const hello = new Item({ // creating deafult data using data model
   name: "Name"
@@ -61,6 +68,35 @@ app.get('/', function(req, res) {
 
 
 });
+
+app.get("/:listid", function(req, res) { // creating custom routs and customlists
+  const customlistname = req.params.listid; // this line of ome from express routing save the value in a variable.
+  const list = new List({ //initiating list model object for custom list
+    name: customlistname,
+    item: defaultitems
+  })
+  List.findOne({ // mongo findone method to check do we have same page or rout
+    name: customlistname
+  }, function(err, result) {
+    if (err) {
+      console.log(err);
+    } else if (!result) { // if we dont have requested route we will create new route and and page using ejs
+      const newList = new List({
+        name: customlistname,
+        item: defaultitems
+      })
+      newList.save(); // the save the default data to data database
+      res.redirect("/" + customlistname); // lastly redirect the page to user after adding the documents to the same page
+    } else {
+      res.render("lists", {
+        listtitle: result.name,
+        item_names: result.item
+      })
+    }
+  })
+
+})
+
 app.post("/", function(req, res) {
   let item = req.body.user_input;
 
@@ -73,24 +109,15 @@ app.post("/", function(req, res) {
   }
 
 });
-app.get("/work", function(req, res) {
-  res.render("lists", {
-    listtitle: "Worklist",
-    item_names: workitems
-  });
 
-})
-app.post("/work", function(req, res) {
-  let item = req.body.user_input;
 
-  res.redirect("/work")
-})
+
 app.post("/delete", function(req, res) {
-  const item_to_be_deleted_by_checked_box = req.body.delete_item;
-  Item.findByIdAndRemove(item_to_be_deleted_by_checked_box,function(err){
-    if(err){
+  const item_to_be_deleted_by_checked_box = req.body.delete_item; // this line of code receive the id checked from ejs stos it
+  Item.findByIdAndRemove(item_to_be_deleted_by_checked_box, function(err) { // mongoose method of deleting an item by its id.
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       console.log("Successfully Deleted");
     }
   })
