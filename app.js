366061a2e1be85923e392baecd2748ed0f176939
asif <mdasifahamed89@yahoo.com>
2022-05-09 14:09:41 +0600

    Removing items from database and also from the list

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose'); // package for dtabase
 const date = require(__dirname + "/date.js"); //  external dat moudule
+const ObjectId = require('mongodb').ObjectID;
 const app = express();
 const port = 3000;
 
@@ -11,33 +12,39 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todolistDB")// setting the database
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}); // setting the database
 const ItemSchema = new mongoose.Schema({ // creatingt data table or for mongoose it says documents format
-  name : String
+  name: String
 });
 const Item = mongoose.model("Item", ItemSchema); // setting the data model
 
 const hello = new Item({ // creating deafult data using data model
-  name : "Name"
+  name: "Name"
 });
 const welcome = new Item({ // creating deafult data using data model
-  name : "Welcome"
+  name: "Welcome"
+});
+const yourtask = new Item({ // creating deafult data using data model
+  name: "Your Task"
+});
+const input_here = new Item({ // creating deafult data using data model
+  name: "Please Input Here"
 });
 
-const defaultitems= [hello, welcome];
+const defaultitems = [hello, welcome, yourtask, input_here];
 
 
 app.get('/', function(req, res) {
-    let day = date.getDay();
+  let day = date.getDay();
 
-  Item.find({},function(err,result){
-    if(err){
+  Item.find({}, function(err, result) {
+    if (err) {
       console.log(err);
-    }else if(result.length === 0){
-      Item.insertMany(defaultitems,function(err,result){ // this else if block will prevebt repetaion database input
-        if(err){
+    } else if (result.length === 0) {
+      Item.insertMany(defaultitems, function(err, result) { // this else if block will prevebt repetaion database input
+        if (err) {
           console.log(err);
-        }else{
+        } else {
           console.log("successfully items added");
         }
       });
@@ -50,7 +57,7 @@ app.get('/', function(req, res) {
       });
     }
 
-    })
+  })
 
 
 });
@@ -78,9 +85,17 @@ app.post("/work", function(req, res) {
 
   res.redirect("/work")
 })
-app.post("/delete",function(req,res){
-  console.log(req.body.delete_item);
-})
+app.post("/delete", function(req, res) {
+  const item_to_be_deleted_by_checked_box = req.body.delete_item;
+  Item.findByIdAndRemove(item_to_be_deleted_by_checked_box,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Successfully Deleted");
+    }
+  })
+  res.redirect("/");
+});
 
 
 app.listen(port, function() {
